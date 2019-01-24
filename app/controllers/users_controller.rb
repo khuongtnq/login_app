db13c234ab485f01239bb98d3ef515698db50dd3
khuongtnq <khuongtnq@gmail.com>
2019-01-24 12:01:00 +0700

    push source login_app to github

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i(new create)
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to user, notisce: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :password, :email, :password_confirmation
+  end
+#  helper_method :user_params
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get     "login"    => "session#new"
+  post    "login"    => "session#create"
+  delete  "logout"   => "session#destroy"
+  resources :users
+end
